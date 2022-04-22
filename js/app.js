14d950259f19186c47af132f511fe3b21b160f6a
zamao9 <94758143+zamao9@users.noreply.github.com>
2022-04-22 11:11:08 +0300

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,16 +1,101 @@
 document.addEventListener('DOMContentLoaded', () => {
 
+  /* BURGER */
+  let burger = document.querySelector('.burger'),
+      navMenu = document.querySelector('.nav_menu'),
+      navlink = document.querySelectorAll('.nav_link'),
+      body = document.getElementsByTagName('body');
+
+  burger.addEventListener('click', (event) => {
+    event.currentTarget.classList.toggle('active');
+    navMenu.classList.toggle('active');
+    body[0].classList.toggle('block');
+  });
+
+  for (let i=0; navlink.length > i; ++i) {
+    navlink[i].addEventListener('click', (event) => {
+      event.preventDefault();
+      navMenu.classList.remove('active');
+      body[0].classList.remove('block');
+      burger.classList.remove('active');
+    })
+  };
+
+
+
+
+  /* LINKS */
+  const navInit = ()   => {
+    const links = document.querySelectorAll('.nav_link'),
+          section = document.querySelectorAll('.section'),
+          intro = document.getElementById('intro');
+
+    section.forEach(section => {
+      if (window.pageYOffset >= section.offsetTop) {
+        links.forEach(link => {
+          link.classList.remove('active');
+          if (link.dataset.section === section.dataset.section) {
+            link.classList.add('active');
+          }
+        })
+      };
+      if (window.pageYOffset < intro.offsetHeight) {
+        links.forEach(link => {
+          link.classList.remove('active')
+        })
+      }
+    })
+  };
+  navInit();
+  window.addEventListener('scroll', () => {
+    navInit();
+  });
+  window.addEventListener('resize', () => {
+    navInit();
+  });
+
+
+
+
+
+
   /* SLIDER_BG */
   const swiperBg = new Swiper('.slider_bg_container', {
     loop: true,
-    autoplay: {
-      delay: 6000
-    },
-    effect: 'fade',
-    fadeEffect: {
-      crossFade: true
-    },
+    parallax: true,
     allowTouchMove: false,
+    autoplay: {
+      delay: 6000,
+    },
+    speed: 2000,
+    breakpoints: {
+      576: {
+        slidesPerView: 2,
+        parallax: false,
+      },
+      992: {
+        slidesPerView: 3,
+        parallax: false,
+      },
+      1200: {
+        slidesPerView: 4,
+        parallax: false,
+      },
+    }
   });
 
+});
+
+$(function() {
+  $("[data-scroll").click(function(event) {
+    event.preventDefault();
+
+    let blockId = $(this).data('scroll'),
+        section = $('.section'),
+        blockOffset = $(blockId).offset().top;
+
+    $("html, body").animate ({
+      scrollTop: blockOffset
+    }, 900);
+  })
 })
