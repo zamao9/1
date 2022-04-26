d391c9b7d5e2e1950dfc7b6b4ef68ea8ac076d03
zamao9 <94758143+zamao9@users.noreply.github.com>
2022-04-26 12:50:37 +0300

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -218,6 +218,33 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   });
 
+
+
+
+  /* AOS */
+  AOS.init({
+  disable: false,
+  startEvent: 'DOMContentLoaded',
+  initClassName: 'aos-init',
+  animatedClassName: 'aos-animate',
+  useClassNames: false,
+  disableMutationObserver: false,
+  debounceDelay: 50, 
+  throttleDelay: 99, 
+  
+  offset: 0, 
+  delay: 0,
+  duration: 1000,
+  easing: 'ease', 
+  once: false,
+  mirror: false, 
+  anchorPlacement: 'bottom-center',
+});
+
+
+
+
+
 });
 
 
